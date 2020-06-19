0b3367cfdc81fe666d2c6798cb952ab1cacd82ab
saurav-07 <msm18b046@iiitdm.ac.in>
2020-06-19 11:38:35 +0530

    Final commit for admin

--- script.js
+++ script.js
@@ -1,157 +1,67 @@
-window.onload = function () {
-
-var chart = new CanvasJS.Chart("chartContainer", {
-	theme:"light2",
-	animationEnabled: true,
-	title:{
-		text: "Game of Thrones Viewers of the First Airing on HBO"
-	},
-	axisY :{
-		includeZero: false,
-		title: "Number of Viewers",
-		suffix: "mn"
-	},
-	toolTip: {
-		shared: "true"
-	},
-	legend:{
-		cursor:"pointer",
-		itemclick : toggleDataSeries
-	},
-	data: [{
-		type: "spline",
-		visible: false,
-		showInLegend: true,
-		yValueFormatString: "##.00mn",
-		name: "Season 1",
-		dataPoints: [
-			{ label: "Ep. 1", y: 2.22 },
-			{ label: "Ep. 2", y: 2.20 },
-			{ label: "Ep. 3", y: 2.44 },
-			{ label: "Ep. 4", y: 2.45 },
-			{ label: "Ep. 5", y: 2.58 },
-			{ label: "Ep. 6", y: 2.44 },
-			{ label: "Ep. 7", y: 2.40 },
-			{ label: "Ep. 8", y: 2.72 },
-			{ label: "Ep. 9", y: 2.66 },
-			{ label: "Ep. 10", y: 3.04 }
-		]
-	},
-	{
-		type: "spline",
-		showInLegend: true,
-		visible: false,
-		yValueFormatString: "##.00mn",
-		name: "Season 2",
-		dataPoints: [
-			{ label: "Ep. 1", y: 3.86 },
-			{ label: "Ep. 2", y: 3.76 },
-			{ label: "Ep. 3", y: 3.77 },
-			{ label: "Ep. 4", y: 3.65 },
-			{ label: "Ep. 5", y: 3.90 },
-			{ label: "Ep. 6", y: 3.88 },
-			{ label: "Ep. 7", y: 3.69 },
-			{ label: "Ep. 8", y: 3.86 },
-			{ label: "Ep. 9", y: 3.38 },
-			{ label: "Ep. 10", y: 4.20 }
-		]
-	},
-	{
-		type: "spline",
-		visible: false,
-		showInLegend: true,
-		yValueFormatString: "##.00mn",
-		name: "Season 3",
-		dataPoints: [
-			{ label: "Ep. 1", y: 4.37 },
-			{ label: "Ep. 2", y: 4.27 },
-			{ label: "Ep. 3", y: 4.72 },
-			{ label: "Ep. 4", y: 4.87 },
-			{ label: "Ep. 5", y: 5.35 },
-			{ label: "Ep. 6", y: 5.50 },
-			{ label: "Ep. 7", y: 4.84 },
-			{ label: "Ep. 8", y: 4.13 },
-			{ label: "Ep. 9", y: 5.22 },
-			{ label: "Ep. 10", y: 5.39 }
-		]
-	},
-	{
-		type: "spline",
-		showInLegend: true,
-		yValueFormatString: "##.00mn",
-		name: "Season 4",
-		dataPoints: [
-			{ label: "Ep. 1", y: 6.64 },
-			{ label: "Ep. 2", y: 6.31 },
-			{ label: "Ep. 3", y: 6.59 },
-			{ label: "Ep. 4", y: 6.95 },
-			{ label: "Ep. 5", y: 7.16 },
-			{ label: "Ep. 6", y: 6.40 },
-			{ label: "Ep. 7", y: 7.20 },
-			{ label: "Ep. 8", y: 7.17 },
-			{ label: "Ep. 9", y: 6.95 },
-			{ label: "Ep. 10", y: 7.09 }
-		]
-	},
-	{
-		type: "spline",
-		showInLegend: true,
-		yValueFormatString: "##.00mn",
-		name: "Season 5",
-		dataPoints: [
-			{ label: "Ep. 1", y: 8 },
-			{ label: "Ep. 2", y: 6.81 },
-			{ label: "Ep. 3", y: 6.71 },
-			{ label: "Ep. 4", y: 6.82 },
-			{ label: "Ep. 5", y: 6.56 },
-			{ label: "Ep. 6", y: 6.24 },
-			{ label: "Ep. 7", y: 5.40 },
-			{ label: "Ep. 8", y: 7.01 },
-			{ label: "Ep. 9", y: 7.14 },
-			{ label: "Ep. 10", y: 8.11 }
-		]
-	},
-	{
-		type: "spline",
-		showInLegend: true,
-		yValueFormatString: "##.00mn",
-		name: "Season 6",
-		dataPoints: [
-			{ label: "Ep. 1", y: 7.94 },
-			{ label: "Ep. 2", y: 7.29 },
-			{ label: "Ep. 3", y: 7.28 },
-			{ label: "Ep. 4", y: 7.82 },
-			{ label: "Ep. 5", y: 7.89 },
-			{ label: "Ep. 6", y: 6.71 },
-			{ label: "Ep. 7", y: 7.80 },
-			{ label: "Ep. 8", y: 7.60 },
-			{ label: "Ep. 9", y: 7.66 },
-			{ label: "Ep. 10", y: 8.89 }
-		]
-	},
-	{
-		type: "spline",
-		showInLegend: true,
-		yValueFormatString: "##.00mn",
-		name: "Season 7",
-		dataPoints: [
-			{ label: "Ep. 1", y: 10.11 },
-			{ label: "Ep. 2", y: 9.27 },
-			{ label: "Ep. 3", y: 9.25 },
-			{ label: "Ep. 4", y: 10.17 },
-			{ label: "Ep. 5", y: 10.72 },
-			{ label: "Ep. 6", y: 10.24 },
-			{ label: "Ep. 7", y: 12.07 }
-		]
-	}]
-});
-chart.render();
-
-function toggleDataSeries(e) {
-	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
-		e.dataSeries.visible = false;
-	} else {
-		e.dataSeries.visible = true;
-	}
-	chart.render();
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {callback: function () { drawChart();$(window).resize(drawChart);},'packages':['corechart', 'line', 'bar']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart1);
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Team');
+    data.addColumn('number', 'Wins');
+    data.addRows([
+      ['CSK', 3],
+      ['MI', 4],
+      ['KKR', 2],
+      ['SRH', 2],
+      ['RR', 1]
+      ]);
+
+    // Set chart options
+    var options = {'title':'Winner of IPL Seasons',
+                  };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+function drawChart1() {
+
+  var data = google.visualization.arrayToDataTable([
+        ['Team', 'Played', 'Won'],
+        ['MI', 187, 109],
+        ['SRH', 183, 100],
+        ['RCB', 180, 92],
+        ['KKR', 178, 87],
+        ['DC', 177, 84],
+        ['KXIP', 176, 82],
+        ['CSK', 164, 77],
+        ['RR', 147, 75]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Matches played by teams and won'
+        },
+        bars: 'vertical' // Required for Material Bar Charts.
+      };
+
+      var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+      chart.draw(data, google.charts.Bar.convertOptions(options));
+ }
